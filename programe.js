44eb7104c610c56b996f1281eeda454d4dfebfe9
Vijayasathvikavempati <sathvikavijju@gmail.com>
2023-11-22 00:17:58 +0530

    first commit

---
+++ programe.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 6;
+console.log(a+b)
+console.log("Good Afternoon-Biryani")
+
+let ename= "Rahul gandhi thathaa";
+console.log(ename)
+
+function Function(){
+    console.log("GM")
+}
+Function()
+
+function Employee_one(){
+    console.log("hai dykme")
+}
+Employee_one()
